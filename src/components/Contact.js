9d042d322e625e1c393d6165fe01dce4e5837770
Jayesh Kushwaha <jayesh.kushwaha25@gmail.com>
2025-01-18 18:33:50 +0530

    Remove feel free

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,6 @@ const Contact = () => {
   return (
     <div id="contact">
       <h2>Contact Me</h2>
-      <p>Feel free to reach out if you'd like to work together or have any questions!</p>
       <p>Email: jayesh.kushwaha@gmail.com</p>
     </div>
   );
