009521a98f17a5aa678103159c3a1742e8a4ff1f
Jayesh Kushwaha <95064038+Jayeshkushwaha@users.noreply.github.com>
2025-01-21 17:43:07 +0530

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -2,14 +2,15 @@ import styles from './FooterStyles.module.css';
 
 function Footer() {
   return (
+    <>
     <section id="footer" className={styles.container}>
-      <div>
       <p>
         &copy; Jayesh Kushwaha. <br />
         All rights reserved.
       </p>
-      </div>
     </section>
+    <div/>
+    </>
   );
 }
 
