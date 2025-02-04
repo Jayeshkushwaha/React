f517240fe5476f1efffb52dd7312b55e7e4e736c
Jayesh Kushwaha <jayesh.kushwaha25@gmail.com>
2025-02-04 14:38:29 +0530

    Icons changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -4,6 +4,8 @@ import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
+import emailLight from '../../assets/email-light.svg';
+import emailDark from '../../assets/email-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
 import CV from '../../assets/cv.pdf';
@@ -14,6 +16,7 @@ function Hero() {
 
   const themeIcon = theme === 'light' ? sun : moon;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const emailIcon = theme === 'light' ? emailLight : emailDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
@@ -24,7 +27,7 @@ function Hero() {
         <img
           src="https://lh3.googleusercontent.com/a/ACg8ocKX78-xhwIiwrYg2uRgUPgNdpahOFw8VWKihLfKBPBWCjv_Fmrm=s576-c-no"
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Jayesh Kushwaha"
         />
         <img
           className={styles.colorMode}
@@ -41,11 +44,14 @@ function Hero() {
         </h1>
         <h2>React Native Developer</h2>
         <span>
+          <a href="https://www.linkedin.com/in/jayesh-kushwaha/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
           <a href="https://github.com/Jayeshkushwaha" target="_blank">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://www.linkedin.com/in/jayesh-kushwaha/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
+          <a href="mailto:jayesh.kushwaha25@gmail.com" target="_blank">
+            <img src={emailIcon} alt="Email icon" />
           </a>
         </span>
         <p className={styles.description}>
